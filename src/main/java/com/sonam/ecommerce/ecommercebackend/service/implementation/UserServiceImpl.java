3fbe2c03cb63bdcebea8a49e720a4be2660161e9
sonamLama22 <lamadsonam19@gmail.com>
2024-03-21 20:12:15 -0400

    bug fix: admin controller

--- src/main/java/com/sonam/ecommerce/ecommercebackend/service/implementation/UserServiceImpl.java
+++ src/main/java/com/sonam/ecommerce/ecommercebackend/service/implementation/UserServiceImpl.java
@@ -1,24 +1,15 @@
 package com.sonam.ecommerce.ecommercebackend.service.implementation;
 
+import com.sonam.ecommerce.ecommercebackend.dto.UserDto;
 import com.sonam.ecommerce.ecommercebackend.entity.User;
 import com.sonam.ecommerce.ecommercebackend.exception.ResourceNotFoundException;
 import com.sonam.ecommerce.ecommercebackend.repository.UserRepo;
 import com.sonam.ecommerce.ecommercebackend.service.UserService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.util.Base64;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
